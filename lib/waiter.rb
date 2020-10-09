96ce6551ece3bde30055c78326530812bb3b0471
DanTheMan114 <danielturner184@gmail.com>
2020-10-09 10:55:41 -0400

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,35 @@
 class Waiter
 
+    @@all = []
+
+    def initialize(name, years)
+        @name = name
+        @years = years
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals 
+        Meal.all.select {|meal|meal.waiter == self}
+    end
+
+    def best_tipper
+        #binding.pry
+        #meals.all
+        meals.select {|meals| meals.tip >= 5}.pop.customer
+        
+    end
+
+
+
+
+
+
 end
